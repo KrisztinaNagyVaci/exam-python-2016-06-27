c806f90089a62bde908b953b86d45c64091972ce
Kriszti <krisztina.vaci@gmail.com>
2016-06-27 09:25:34 +0200

    task 1

--- first.py
+++ first.py
@@ -3,3 +3,16 @@
 # It should raise an error if the parameter is not a list
 # example: [1, 2, 3, 4, 5] should produce [2, 4]
 
+l = [1, 2, 3, 4, 5]
+a = 'string'
+
+def selectEverySecond(input):
+    newList = []
+    if isinstance(input, list):
+        for item in input[1::2]:
+            newList.append(item)
+        return (newList)
+    else:
+        return 'The parameter is not a list'
+
+print(selectEverySecond(l))
