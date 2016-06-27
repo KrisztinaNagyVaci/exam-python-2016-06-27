301a896576a586886247e31c7a685941e4c32dfc
Kriszti <krisztina.vaci@gmail.com>
2016-06-27 10:49:04 +0200

    Task 4 launch works

--- fourth.py
+++ fourth.py
@@ -3,7 +3,29 @@
 # 1st parameter: the type of the rocket as a string, "falcon1" or "falcon9"
 # 2nd parameter: the starting fuel level as a number
 # 3rd parameter: number of launches as a number
-#
+
+class Rocket:
+
+    def __init__ (self, rocket, fuel, launches):
+        self.rocket = rocket
+        self.fuel = fuel
+        self.launches = launches
+
+    def launch(self):
+        if self.rocket == 'falcon1':
+            self.fuel = self.fuel - 1
+            return self.fuel
+        if self.rocket == 'falcon9':
+            self.fuel = self.fuel - 9
+            return self.fuel
+
+
+rocket1 = Rocket('falcon1', 3, 2)
+print(rocket1.launch())
+
+rocket2 = Rocket('falcon9', 10, 2)
+print(rocket2.launch())
+
 # It should have 3 methods:
 #
 # launch()
@@ -43,21 +65,21 @@
 # The following code should work with the classes
 
 
-space_x = SpaceX(100)
-falcon1 = Rocket('falcon1', 0, 0)
-falcon9 = Rocket('falcon9', 0, 0)
-returned_falcon9 = Rocket('falcon9', 11, 1)
+#space_x = SpaceX(100)
+#falcon1 = Rocket('falcon1', 0, 0)
+#falcon9 = Rocket('falcon9', 0, 0)
+#returned_falcon9 = Rocket('falcon9', 11, 1)
 
-print(returned_falcon9.getStats()) # name: falcon9, fuel: 11
+#print(returned_falcon9.getStats()) # name: falcon9, fuel: 11
 
-space_x.addRocket(falcon1)
-space_x.addRocket(falcon9)
-space_x.addRocket(returned_falcon9)
+#space_x.addRocket(falcon1)
+#space_x.addRocket(falcon9)
+#space_x.addRocket(returned_falcon9)
 
-print(space_x.getStats()) # rockets: 3, fuel: 100, launches: 1
-space_x.refill_all()
-print(space_x.getStats()) # rockets: 3, fuel: 66, launches: 1
-space_x.launch_all()
-print(space_x.getStats()) # rockets: 3, fuel: 66, launches: 4
-space_x.buy_fuel(50)
-print(space_x.getStats()) # rockets: 3, fuel: 116, launches: 4
+#print(space_x.getStats()) # rockets: 3, fuel: 100, launches: 1
+#space_x.refill_all()
+#print(space_x.getStats()) # rockets: 3, fuel: 66, launches: 1
+#space_x.launch_all()
+#print(space_x.getStats()) # rockets: 3, fuel: 66, launches: 4
+#space_x.buy_fuel(50)
+#print(space_x.getStats()) # rockets: 3, fuel: 116, launches: 4
